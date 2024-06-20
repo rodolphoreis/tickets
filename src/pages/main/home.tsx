38531babc5b5272cce9ac1f951c256a14b558c4b
Rodolpho Reis <rodolphoreis@live.com>
2024-06-20 15:44:31 +0100

    feat: add news cards

--- src/pages/main/home.tsx
+++ src/pages/main/home.tsx
@@ -17,6 +17,34 @@ const home = () => {
             </p>
           </div>
         </div>
+        <div className="bg-white shadow-lg rounded-lg">
+          <img src="" alt="" />
+          <div className="p-4">
+            <h2 className="text-xl text-black font-bold">Show do Radiohead</h2>
+            <p className="text-gray-700 mt-2">Sab, 15/07/2024</p>
+            <p className="text-gray-700 mt-2">Tem 10 lugares disponíveis</p>
+            <p className="text-gray-700 mt-2">€ 120</p>
+            <p>
+              <button className="rounded-lg bg-gray-800 px-4 py-1 mt-3">
+                Reservar lugar
+              </button>
+            </p>
+          </div>
+        </div>
+        <div className="bg-white shadow-lg rounded-lg">
+          <img src="" alt="" />
+          <div className="p-4">
+            <h2 className="text-xl text-black font-bold">Show do Radiohead</h2>
+            <p className="text-gray-700 mt-2">Sab, 15/07/2024</p>
+            <p className="text-gray-700 mt-2">Tem 10 lugares disponíveis</p>
+            <p className="text-gray-700 mt-2">€ 120</p>
+            <p>
+              <button className="rounded-lg bg-gray-800 px-4 py-1 mt-3">
+                Reservar lugar
+              </button>
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
