d54a7ded136bdd114d2c8314a594137d9d930300
Rodolpho Reis <rodolphoreis@live.com>
2024-06-24 17:51:20 +0100

    feat: remove cards

--- src/pages/main/home.tsx
+++ src/pages/main/home.tsx
@@ -25,37 +25,9 @@ const Home = () => {
             </p>
           </div>
         </div>
-        <div className="bg-white shadow-lg rounded-lg">
-          <img src="" alt="" />
-          <div className="p-4">
-            <h2 className="text-xl text-black font-bold">Show do Radiohead</h2>
-            <p className="text-gray-700 mt-2">Sab, 15/07/2024</p>
-            <p className="text-gray-700 mt-2">Tem 10 lugares disponíveis</p>
-            <p className="text-gray-700 mt-2">€ 120</p>
-            <p>
-              <button className="rounded-lg bg-gray-800 px-4 py-1 mt-3 hover:bg-gray-700">
-                Reservar lugar
-              </button>
-            </p>
-          </div>
-        </div>
-        <div className="bg-white shadow-lg rounded-lg">
-          <img src="" alt="" />
-          <div className="p-4">
-            <h2 className="text-xl text-black font-bold">Show do Radiohead</h2>
-            <p className="text-gray-700 mt-2">Sab, 15/07/2024</p>
-            <p className="text-gray-700 mt-2">Tem 10 lugares disponíveis</p>
-            <p className="text-gray-700 mt-2">€ 120</p>
-            <p>
-              <button className="rounded-lg bg-gray-800 px-4 py-1 mt-3 hover:bg-gray-700">
-                Reservar lugar
-              </button>
-            </p>
-          </div>
-        </div>
       </div>
     </main>
   );
 };
 
-export default home;
+export default Home;
