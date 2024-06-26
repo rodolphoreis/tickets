ede3a9cca34eb8b3eea72993d0224702ca6a900c
Rodolpho Reis <rodolphoreis@live.com>
2024-06-26 17:39:43 +0100

    feat: create function getEvent

--- src/app/events/[eventId]/spots-layout/page.tsx
+++ src/app/events/[eventId]/spots-layout/page.tsx
@@ -1,7 +1,23 @@
+import { EventModel, SpotModel } from "../../../../../models";
+
+async function getEvent(
+  eventId: number
+): Promise<{ event: EventModel; spots: SpotModel[] }> {
+  const response = await fetch(`http://localhost:8000/events/${eventId}`, {
+    cache: "no-store",
+    next: {
+      tags: [`events/${eventId}`],
+    },
+  });
+  return response.json();
+}
+
 export default async function SpotsLayoutPage({
   params,
 }: {
   params: { eventId: string };
 }) {
+  const { event, spots } = await getEvent(parseInt(params.eventId));
+
   return <p>{params.eventId}</p>;
 }
